07be444f4f38ef90a088dba22604b6ce91653a1a
Yupiel <stefan_mitrovic@outlook.com>
2021-07-12 16:17:24 +0200

    fix scheduled-spotify-title-grabber lambda

--- lambdas/scheduled-spotify-title-grabber/handler.ts
+++ lambdas/scheduled-spotify-title-grabber/handler.ts
@@ -1,3 +1,4 @@
+import dateFormat from 'dateformat';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import SpotifyWebApi from 'spotify-web-api-node';
 import {
@@ -15,22 +16,28 @@ const s3Client = new S3Client({
 	region: AWS_REGION,
 });
 
+const searchYear = 2020;
+
 export const grab = async (event: APIGatewayProxyEvent) => {
-	const spotifyCredentials = await secretsManagerClient
+	console.log('Authenticating against Spotify API...');
+	const spotifySecret = await secretsManagerClient
 		.send(
 			new GetSecretValueCommand({
 				SecretId:
 					'arn:aws:secretsmanager:eu-central-1:277817539157:secret:spotify_secrets-t94O8R',
 			})
 		)
-		.then((res) => {
-			if (res.SecretString !== undefined && res.SecretString !== '')
-				return JSON.parse(res.SecretString);
-			else
-				throw new Error(
-					'Spotify Credentials could not be obtained from Secret'
-				);
-		});
+		.then((res) => res);
+
+	if (
+		spotifySecret.SecretString === undefined ||
+		spotifySecret.SecretString === ''
+	)
+		throw new Error(
+			'Spotify Credentials could not be obtained from Secret'
+		);
+
+	const spotifyCredentials = JSON.parse(spotifySecret.SecretString);
 
 	const spotifyClient = new SpotifyWebApi({
 		clientId: spotifyCredentials['spotify-api-client-id'],
@@ -43,32 +50,28 @@ export const grab = async (event: APIGatewayProxyEvent) => {
 
 	let spotifySearchOffset = 0;
 
+	console.log('Fetching data from Spotify API...');
 	while (true) {
 		try {
-			let songNameList: string[] = [];
-
-			await spotifyClient
-				.searchTracks('year:2020', {
+			const spotifyDataResponse = await spotifyClient
+				.searchTracks(`year:${searchYear}`, {
 					limit: 50,
 					offset: spotifySearchOffset,
 					market: 'DE',
 				})
-				.then((data) => {
-					if (data.body.tracks?.items === undefined)
-						throw new Error('No tracks found in response');
+				.then((data) => data);
 
-					for (let track of data.body.tracks?.items) {
-						songNameList.push(track.name);
-					}
-				});
+			if (spotifyDataResponse === undefined)
+				throw new Error('No tracks found in response');
 
 			await s3Client.send(
 				new PutObjectCommand({
-					Bucket: 'frhetorix-analysis',
-					Key: `search_${2020}_DE_track_${new Date()
-						.toLocaleDateString()
-						.replaceAll('/', '-')}_${spotifySearchOffset}.json`,
-					Body: JSON.stringify(songNameList),
+					Bucket: 'frhetorix-raw-data',
+					Key: `search_${searchYear}_DE_track_${dateFormat(
+						new Date(),
+						'dd-mm-yyyy'
+					)}_${spotifySearchOffset}.json`,
+					Body: JSON.stringify(spotifyDataResponse),
 					ContentType: 'text/plain',
 				})
 			);
@@ -79,4 +82,8 @@ export const grab = async (event: APIGatewayProxyEvent) => {
 			break;
 		}
 	}
+
+	console.log(
+		`Finishing up... wrote ${spotifySearchOffset / 50} files to s3.`
+	);
 };
